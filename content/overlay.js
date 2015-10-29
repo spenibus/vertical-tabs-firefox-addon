18ebe008ff314904a954d49cfa0f9e601e122414
spenibus <git@spenibus.net>
2015-10-29 06:58:55 +0100

    quicksave

--- content/overlay.js
+++ content/overlay.js
@@ -1,194 +1,145 @@
-/******************************************************************************/
-spenibus_verticalTabs = {
+//*********** export named single global object while using shorthand internally
+var spenibus_verticalTabs = (function() {
 
 
-    /**************************************************************************/
-    init : function() {
+
+
+    //************************************************************** run on load
+    window.addEventListener('load', (function f(){
 
         // remove init listener
-        window.removeEventListener('load', spenibus_verticalTabs.init, false);
+        window.removeEventListener('load', f, false);
+
+        // load style
+        s.loadStyle();
 
         // update gui to show vertical tab bar
-        spenibus_verticalTabs.windowUpdate();
-    },
+        s.windowUpdate();
+
+    }), false);
 
 
 
 
-    /**************************************************************************/
-    windowUpdate : function() {
+    //******************************************************* internal shorthand
+    var s = {};
+
+
+
+
+    //******************************************************************** prefs
+    s.prefs = {};
+
+
+    s.prefs.service = Components.classes["@mozilla.org/preferences-service;1"]
+        .getService(Components.interfaces.nsIPrefService)
+        .getBranch("extensions.spenibus_verticalTabs.");
+
+
+    s.prefs.funcs = {
+        '0'   : function(){return null;},
+        '32'  : s.prefs.service.getCharPref,
+        '64'  : s.prefs.service.getIntPref,
+        '128' : s.prefs.service.getBoolPref,
+    }
+
+
+    s.prefs.get = function(n) {
+        var t = s.prefs.service.getPrefType(n);
+        var f = s.prefs.funcs[t];
+        return f(n);
+    };
+
+
+
+
+    //********************************************************* show/hide tabbar
+    s.showHide = function() {
+
+        var hidden = !this.node_container.hidden;
+
+        this.node_container.hidden = hidden;
+        this.node_splitter.hidden  = hidden;
+    };
+
+
+
+    //*************************************************************** load style
+    s.loadStyle = function() {
+
+        // get browser
+        var node = document.getElementById('browser');
+
+
+        // compact style
+        if(s.prefs.get('style_compact_enable')) {
+            node.classList.add('spenibus_verticalTabs_compact');
+        }
+    };
+
+
+
+
+    //**************************************************************************
+    s.windowUpdate = function() {
 
         // addon elements
-        this.node_container     = document.getElementById('spenibus_verticalTabs_container');
-        this.node_buttons       = document.getElementById('spenibus_verticalTabs_buttons');
-        this.node_closeButton   = document.getElementById('spenibus_verticalTabs_closeButton');
-        this.node_splitter      = document.getElementById('spenibus_verticalTabs_splitter');
+        s.node_container   = document.getElementById('spenibus_verticalTabs_container');
+        s.node_buttons     = document.getElementById('spenibus_verticalTabs_buttons');
+        s.node_closeButton = document.getElementById('spenibus_verticalTabs_closeButton');
+        s.node_splitter    = document.getElementById('spenibus_verticalTabs_splitter');
 
         // native elements
-        this.node_tabsToolbar    = document.getElementById('TabsToolbar');
-        this.node_tabbrowsertabs = document.getElementById('tabbrowser-tabs');
+        s.node_tabsToolbar    = document.getElementById('TabsToolbar');
+        s.node_tabbrowsertabs = document.getElementById('tabbrowser-tabs');
 
         // native buttons
-        this.node_newtabbutton  = document.getElementById('new-tab-button');
-        this.node_alltabsbutton = document.getElementById('alltabs-button');
+        s.node_newtabbutton  = document.getElementById('new-tab-button');
+        s.node_alltabsbutton = document.getElementById('alltabs-button');
 
 
 
 
         // group buttons horizontally
-        this.node_buttons.appendChild(this.node_alltabsbutton);
-        this.node_buttons.appendChild(this.node_newtabbutton);
-        this.node_buttons.appendChild(this.node_closeButton);
+        s.node_buttons.appendChild(s.node_alltabsbutton);
+        s.node_buttons.appendChild(s.node_newtabbutton);
+        s.node_buttons.appendChild(s.node_closeButton);
 
         // move buttons holder inside tabs toolbar
-        this.node_tabbrowsertabs.parentNode.insertBefore(
-            this.node_buttons,
-            this.node_tabbrowsertabs.nextSibling
+        s.node_tabbrowsertabs.parentNode.insertBefore(
+            s.node_buttons,
+            s.node_tabbrowsertabs.nextSibling
         );
 
 
 
 
         // put tab bar in vertical box
-        this.node_container.appendChild(this.node_tabsToolbar);
+        s.node_container.appendChild(s.node_tabsToolbar);
 
         // orient tab strip vertically
-        this.node_tabbrowsertabs.mTabstrip.setAttribute('orient', 'vertical');
+        s.node_tabbrowsertabs.mTabstrip.setAttribute('orient', 'vertical');
 
         // make tabs toolbar fill its container
-        this.node_tabsToolbar.setAttribute('flex', '1');
+        s.node_tabsToolbar.setAttribute('flex', '1');
 
         // orient tabs toolbar vertically
-        this.node_tabsToolbar.setAttribute('orient', 'vertical');
+        s.node_tabsToolbar.setAttribute('orient', 'vertical');
 
         // reverse elements order within tabs toolbar
-        this.node_tabsToolbar.setAttribute('dir', 'reverse');
+        s.node_tabsToolbar.setAttribute('dir', 'reverse');
 
         // assign contextual menu to cover entire vertical tab bar
-        this.node_tabbrowsertabs.setAttribute('context', 'tabContextMenu');
-        this.node_buttons.setAttribute('context', 'tabContextMenu');
+        s.node_tabbrowsertabs.setAttribute('context', 'tabContextMenu');
+        s.node_buttons.setAttribute('context', 'tabContextMenu');
 
 
 
 
-        // native override: this will put the dragged tab in place of the hovered tab (also x/y width/height)
-        this.node_tabbrowsertabs._getDropIndex = function(event) {
-
-            var tabs = this.childNodes;
-            var tab  = this._getDragTargetTab(event);
-
-            for(let i=0; i<tabs.length; ++i) {
-                if(event.screenY < tabs[i].boxObject.screenY + tabs[i].boxObject.height) {
-                    if(this.selectedIndex < i) {
-                        ++i;
-                    }
-                    return i;
-                }
-            }
-            return tabs.length;
-        }
-
-
-
-
-        // native override: target tab for link drop
-        this.node_tabbrowsertabs._getDragTargetTab = function(event) {
-
-            var tab = event.target.localName == "tab" ? event.target : null;
-
-            if(
-                tab
-                && (event.type == "drop" || event.type == "dragover")
-                && event.dataTransfer.dropEffect == "link"
-            ) {
-
-                let boxObject = tab.boxObject;
-
-                if(
-                    event.screenY < boxObject.screenY + boxObject.height * 0
-                    || event.screenY > boxObject.screenY + boxObject.height * 1
-                ) {
-                    return null;
-                }
-            }
-
-            return tab;
-        }
-
-
-
-
-        // native override: skip the animation, just get drop index
-        this.node_tabbrowsertabs._animateTabMove = function(event) {
-
-            // dragged tab
-            let draggedTab = event.dataTransfer.mozGetDataAt(TAB_DROP_TYPE, 0);
-
-            // drop index
-            let newIndex = spenibus_verticalTabs.node_tabbrowsertabs._getDropIndex(event);
-
-            // set drop index
-            draggedTab._dragData.animDropIndex = newIndex;
-
-
-            // animation: prepare
-            let screenY   = event.screenY;
-            let tabHeight = draggedTab.getBoundingClientRect().height;
-            let pinned    = draggedTab.pinned;
-            let numPinned = this.tabbrowser._numPinnedTabs;
-
-
-            // tabs list
-            let tabs = this.tabbrowser.visibleTabs.slice(
-                pinned ? 0 : numPinned,
-                pinned ? numPinned : undefined
-            );
-
-
-            // from orignal function, seems to reset tabs position when animation is done
-            if(this.getAttribute("movingtab") != "true") {
-                this.setAttribute("movingtab", "true");
-                this.selectedItem = draggedTab;
-            }
-
-
-            // func: get tab shift
-            function getTabShift(tab, dropIndex) {
-                if(tab._tPos < draggedTab._tPos && tab._tPos >= dropIndex) {
-                    return tabHeight;
-                }
-                if(tab._tPos > draggedTab._tPos && tab._tPos < dropIndex) {
-                    return -tabHeight;
-                }
-                return 0;
-            }
-
-
-            //******************************* animation: move tabs to create gap
-            for(let tab of tabs) {
-                if(tab != draggedTab) {
-                    let shift = getTabShift(tab, newIndex);
-                    tab.style.transform = "translateY(" + shift + "px)";
-                }
-            }
-
-
-            //************************************** animation: move dragged tab
-            // center dragged tab on mouse
-            let tabCenter  = draggedTab.boxObject.screenY + draggedTab.boxObject.height/2;
-            let translateY = screenY - tabCenter;
-
-            // translation boundaries
-            let translateYmin = tabs[0].boxObject.y             - draggedTab.boxObject.y;
-            let translateYmax = tabs[tabs.length-1].boxObject.y - draggedTab.boxObject.y;
-
-            // keep within boundaries
-            translateY = Math.max(translateY, translateYmin);
-            translateY = Math.min(translateY, translateYmax);
-
-            // move tab
-            draggedTab.style.transform = "translateY(" + translateY + "px)";
-        }
+        // native overrides
+        s.node_tabbrowsertabs._getDropIndex     = s.overrides._getDropIndex;
+        s.node_tabbrowsertabs._getDragTargetTab = s.overrides._getDragTargetTab;
+        s.node_tabbrowsertabs._animateTabMove   = s.overrides._animateTabMove;
 
 
 
@@ -200,44 +151,145 @@ spenibus_verticalTabs = {
             var fs = document.mozFullScreen ? 'true' : 'false';
 
             this.node_container.setAttribute('collapsed', fs);
-            this.node_splitter.setAttribute('collapsed', fs);
+            this.node_splitter.setAttribute('collapsed',  fs);
 
-        }.bind(this), false);
-    },
-
-
-
-
-    /**************************************************************************/
-    showHide : function() {
-
-        var hidden = !this.node_container.hidden;
-
-        this.node_container.hidden = hidden;
-        this.node_splitter.hidden  = hidden;
-    },
+        }.bind(s), false);
+    };
 
 
 
 
-    /**************************************************************************/
-    loadStyle : function() {
-
-        var node = document.getElementById('#browser');
-
-
-/*
-use an extra class "enabled" on tabbar
-*/
-
-
-    },
-
-
-};
+    //********************************************************* native overrides
+    s.overrides = {};
 
 
 
 
-/**************************************************************** run on load */
-window.addEventListener('load', spenibus_verticalTabs.init, false);
+    //**************** this will put the dragged tab in place of the hovered tab
+    // while fixing x/y and width/height
+    s.overrides._getDropIndex = function(event) {
+
+        var tabs = this.childNodes;
+        var tab  = this._getDragTargetTab(event);
+
+        for(let i=0; i<tabs.length; ++i) {
+            if(event.screenY < tabs[i].boxObject.screenY + tabs[i].boxObject.height) {
+                if(this.selectedIndex < i) {
+                    ++i;
+                }
+                return i;
+            }
+        }
+        return tabs.length;
+    }
+
+
+
+
+    //************************************************* target tab for link drop
+    s.overrides._getDragTargetTab = function(event) {
+
+        var tab = event.target.localName == "tab" ? event.target : null;
+
+        if(
+            tab
+            && (event.type == "drop" || event.type == "dragover")
+            && event.dataTransfer.dropEffect == "link"
+        ) {
+
+            let boxObject = tab.boxObject;
+
+            if(
+                event.screenY < boxObject.screenY + boxObject.height * 0
+                || event.screenY > boxObject.screenY + boxObject.height * 1
+            ) {
+                return null;
+            }
+        }
+
+        return tab;
+    }
+
+
+
+
+    //********************************** skip the animation, just get drop index
+    s.overrides._animateTabMove = function(event) {
+
+        // dragged tab
+        let draggedTab = event.dataTransfer.mozGetDataAt(TAB_DROP_TYPE, 0);
+
+        // drop index
+        let newIndex = spenibus_verticalTabs.node_tabbrowsertabs._getDropIndex(event);
+
+        // set drop index
+        draggedTab._dragData.animDropIndex = newIndex;
+
+
+        // animation: prepare
+        let screenY   = event.screenY;
+        let tabHeight = draggedTab.getBoundingClientRect().height;
+        let pinned    = draggedTab.pinned;
+        let numPinned = this.tabbrowser._numPinnedTabs;
+
+
+        // tabs list
+        let tabs = this.tabbrowser.visibleTabs.slice(
+            pinned ? 0 : numPinned,
+            pinned ? numPinned : undefined
+        );
+
+
+        // from orignal function, seems to reset tabs position when animation is done
+        if(this.getAttribute("movingtab") != "true") {
+            this.setAttribute("movingtab", "true");
+            this.selectedItem = draggedTab;
+        }
+
+
+        // func: get tab shift
+        function getTabShift(tab, dropIndex) {
+            if(tab._tPos < draggedTab._tPos && tab._tPos >= dropIndex) {
+                return tabHeight;
+            }
+            if(tab._tPos > draggedTab._tPos && tab._tPos < dropIndex) {
+                return -tabHeight;
+            }
+            return 0;
+        }
+
+
+        //*********************************** animation: move tabs to create gap
+        for(let tab of tabs) {
+            if(tab != draggedTab) {
+                let shift = getTabShift(tab, newIndex);
+                tab.style.transform = "translateY(" + shift + "px)";
+            }
+        }
+
+
+        //****************************************** animation: move dragged tab
+        // center dragged tab on mouse
+        let tabCenter  = draggedTab.boxObject.screenY + draggedTab.boxObject.height/2;
+        let translateY = screenY - tabCenter;
+
+        // translation boundaries
+        let translateYmin = tabs[0].boxObject.y             - draggedTab.boxObject.y;
+        let translateYmax = tabs[tabs.length-1].boxObject.y - draggedTab.boxObject.y;
+
+        // keep within boundaries
+        translateY = Math.max(translateY, translateYmin);
+        translateY = Math.min(translateY, translateYmax);
+
+        // move tab
+        draggedTab.style.transform = "translateY(" + translateY + "px)";
+    }
+
+
+
+
+    // export
+    return s;
+
+
+})();
